aa2462b3d7f18e177070c88ec33134bebe1a8cbb
FearandGreed <31392616+FearandGreed@users.noreply.github.com>
2021-07-15 00:17:10 -0400

    updated

---
+++ blog/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import Post, Category
+
+#Create querry list
+categorylist = Category.objects.all().values_list('name','name')
+cat_list = []
+for item in categorylist:
+	cat_list.append(item)
+
+class PostForm(forms.ModelForm):
+	class Meta:
+		model = Post
+		fields = ('title', 'title_tag', 'author', 'category', 'body', 'snippet')
+
+		widgets = {
+			'title': forms.TextInput(attrs={'class': 'col-12 form-group', 'placeholder': cat_list}),
+			'title_tag': forms.TextInput(attrs={'class': 'col-md-6 form-group'}),
+			'author': forms.Select(attrs={'class': 'col-md-6 form-group'}),
+			'category': forms.Select(choices = cat_list, attrs={'class': 'col-md-6 form-group'}),
+			'body': forms.Textarea(attrs={'class': 'col-12 form-group'}),
+			'snippet': forms.Textarea(attrs={'class': 'col-12 form-group'}),
+		}
+
+class EditForm(forms.ModelForm):
+	class Meta:
+		model = Post
+		fields = ('title', 'title_tag', 'author', 'category', 'body', 'snippet')
+
+		widgets = {
+			'title': forms.TextInput(attrs={'class': 'col-12 form-group'}),
+			'title_tag': forms.TextInput(attrs={'class': 'col-md-6 form-group'}),
+			'category': forms.Select(attrs={'class': 'col-md-6 form-group'}),
+			'author': forms.Select(attrs={'class': 'col-md-6 form-group'}),
+			'category': forms.Select(choices = cat_list, attrs={'class': 'col-md-6 form-group'}),
+			'body': forms.Textarea(attrs={'class': 'col-12 form-group'}),
+			'snippet': forms.Textarea(attrs={'class': 'col-12 form-group'}),
+		}
---
+++ venv_fearandgreed.bat
@@ -0,0 +1,9 @@
+Powershell.exe -noexit -executionpolicy remotesigned -File  d:\Dropbox/code/venv_fearandgreed.ps1
+#start powershell.exe -noexit
+#cmd
+#@echo off
+#echo Starting virtual enviroment
+#start "" d:/dropbox/code/venvs/venv_dashboard/scripts/activate
+#echo Done
+#cmd /k
+#d:/dropbox/code/dashboard/
